37ec797f975782f8484f73ddfd67fb9066d838bf
Mustafa Özgün <mustiozgun@gmail.com>
2020-07-24 19:22:02 +0300

    header güncellendi

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,27 @@
 import React from "react";
-import logo from '../../logo.png';
-
+import logo from "../../logo.png";
+import { Link, NavLink } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
 
 function header() {
+  return (
+    <Menu stackable>
+      <Menu.Item>
+        <img src={logo} className="logo" alt="" />
+      </Menu.Item>
 
-    return (
-        <header>
-          <a href="/" className="logo">
-            <img src={logo} className="logo" alt="" />
-          </a>
+      <Menu.Item name="features">
+        <NavLink to="/">Anasayfa</NavLink> 
+      </Menu.Item>
 
-        </header>
-    );
+      <Menu.Item name="features">
+        <NavLink to="/createArticle">Yeni Yazı </NavLink> 
+      </Menu.Item>
 
+
+
+    </Menu>
+  );
 }
 
 export default header;
